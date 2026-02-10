6c80d7f6f125513fccdf619468f4e08b761a896e
princepatel2002 <prince33d28july2002@gmail.com>
2026-02-11 02:50:20 +0700

    login complete

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,8 +31,31 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send('User login endpoint');
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if(!email || !password){
+            return res.status(400).json({ message: "All fields are required", status:"failed" });
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials", status:"failed" });
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials", status:"failed" });
+        }
+        const token = jwt.sign ({userId : user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+    
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production'});
+            return res.status(200).json({ message: "User logged in successfully", user:{userId: user._id,name:user.name,email:user.email}, token, status:"success" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", status:"failed", error: error.message });
+    }
+
 }
 
 export const logout = (req, res) => {
